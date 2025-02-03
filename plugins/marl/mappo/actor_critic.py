4ddfa5f352323d1714633fbb3b70d92393361bdf
wecracy <wecracy@gmail.com>
2025-02-03 15:28:51 +0900

    First Commit of MAPPO Policy Plugins

---
+++ plugins/marl/mappo/actor_critic.py
@@ -0,0 +1,210 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+# The code is based on the MAPPO implementation from the repository: https://github.com/marlbenchmark/on-policy/
+
+def get_shape_from_obs_space(obs_space):
+    if obs_space.__class__.__name__ == 'Box':
+        obs_shape = obs_space.shape
+    elif obs_space.__class__.__name__ == 'list':
+        obs_shape = obs_space
+    else:
+        raise NotImplementedError
+    return obs_shape
+
+def init(module, weight_init, bias_init, gain=1):
+    weight_init(module.weight.data, gain=gain)
+    if module.bias is not None:
+        bias_init(module.bias.data)
+    return module
+
+class RNNLayer(nn.Module):
+    def __init__(self, inputs_dim, outputs_dim, recurrent_N):
+        super(RNNLayer, self).__init__()
+        self._recurrent_N = recurrent_N
+        self.input_dim = inputs_dim
+
+        self.rnn = nn.GRU(inputs_dim, outputs_dim, num_layers=self._recurrent_N)
+        for name, param in self.rnn.named_parameters():
+            if 'bias' in name:
+                nn.init.constant_(param, 0)
+            elif 'weight' in name:
+                nn.init.orthogonal_(param)
+        self.norm = nn.LayerNorm(outputs_dim)
+
+    def forward(self, x, hxs):#, masks):
+        hxs = torch.tensor(hxs, dtype=torch.float32)
+        #masks = torch.tensor(masks, dtype=torch.float32)
+        if x.dim() == 1:
+            x = x.unsqueeze(0)
+        if x.size(0) == hxs.size(0):
+            x, hxs = self.rnn(x, hxs)
+            x = x.squeeze(0)
+        else:
+            # x is a (T, N, -1) tensor that has been flatten to (T * N, -1)
+            N = hxs.size(0)
+            T = int(x.size(0) / N)
+
+            # unflatten
+            x = x.view(T, N, x.size(1))
+
+            # x is a (T, N, -1) tensor
+            hxs = hxs.transpose(1,0)
+            rnn_scores, hxs = self.rnn(x, hxs)
+
+            # flatten
+            x = rnn_scores.reshape(T * N, -1)
+            hxs = hxs.transpose(0, 1)
+
+        x = self.norm(x)
+        return x, hxs
+
+class FixedCategorical(torch.distributions.Categorical):
+    def sample(self):
+        return super().sample().unsqueeze(-1)
+
+    def log_probs(self, actions):
+        return (
+            super()
+            .log_prob(actions.squeeze(-1))
+            .view(actions.size(0), -1)
+            .sum(-1)
+            .unsqueeze(-1)
+        )
+
+class Categorical(nn.Module):
+    def __init__(self, num_inputs, num_outputs, gain=0.01):
+        super(Categorical, self).__init__()
+        init_method = nn.init.orthogonal_
+        def init_(m):
+            return init(m, init_method, lambda x: nn.init.constant_(x, 0), gain)
+
+        self.linear = init_(nn.Linear(num_inputs, num_outputs))
+
+    def forward(self, x, action_masks = None):
+        x = self.linear(x)
+        if action_masks is not None:
+            x[action_masks == 0] = -1e10
+        return FixedCategorical(logits=x)
+
+class ACTLayer(nn.Module):
+    def __init__(self, action_space, inputs_dim):
+        super(ACTLayer, self).__init__()
+        self.action_type = action_space.__class__.__name__
+
+        action_dim = action_space.n
+        self.action_out = Categorical(inputs_dim, action_dim)
+
+    def forward(self, x, action_masks=None):
+        action_logits = self.action_out(x, action_masks)
+        actions = action_logits.sample()
+        action_log_probs = action_logits.log_probs(actions)
+
+        return actions, action_log_probs
+
+    def get_probs(self, x, available_actions=None):
+        action_logits = self.action_out(x, available_actions)
+        action_probs = action_logits.probs
+
+        return action_probs
+
+    def evaluate_actions(self, x, action, available_actions=None):
+        action_logits = self.action_out(x, available_actions)
+        action_log_probs = action_logits.log_probs(action)
+        dist_entropy = action_logits.entropy().mean()
+
+        return action_log_probs, dist_entropy
+
+class MLPBase(nn.Module):
+    def __init__(self, hidden_size, obs_shape, layer_N):
+        super(MLPBase, self).__init__()
+        obs_dim = obs_shape[0]
+        self.feature_norm = nn.LayerNorm(obs_dim)
+        self.mlp = MLPLayer(obs_dim, hidden_size, layer_N)
+
+    def forward(self, x):
+        x = np.array(x, dtype=np.float32)
+        x = torch.tensor(x, dtype=torch.float32)
+        x = self.feature_norm(torch.tensor(x))
+        x = self.mlp(x)
+
+        return x
+
+class MLPLayer(nn.Module):
+    def __init__(self, input_dim, hidden_size, layer_N):
+        super(MLPLayer, self).__init__()
+        self._layer_N = layer_N
+
+        active_func = nn.ReLU()
+        init_method = nn.init.orthogonal_
+        gain = nn.init.calculate_gain('relu')
+
+        def init_(m):
+            return init(m, init_method, lambda x: nn.init.constant_(x, 0), gain=gain)
+
+        self.fc1 = nn.Sequential(
+            init_(nn.Linear(input_dim, hidden_size)), active_func, nn.LayerNorm(hidden_size))
+        self.fc2 = nn.ModuleList([nn.Sequential(init_(
+            nn.Linear(hidden_size, hidden_size)), active_func, nn.LayerNorm(hidden_size)) for i in range(self._layer_N)])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        for i in range(self._layer_N):
+            x = self.fc2[i](x)
+        return x
+
+class Actor(nn.Module):
+    def __init__(self, hidden_size, layer_N, recurrent_N, obs_space, action_space, device=torch.device("cpu")):
+        super(Actor, self).__init__()
+        self.hidden_size = hidden_size
+
+        obs_shape = get_shape_from_obs_space(obs_space)
+
+        self.base = MLPBase(hidden_size, obs_shape, layer_N)
+        self.rnn = RNNLayer(hidden_size, hidden_size, recurrent_N)
+        self.act = ACTLayer(action_space, hidden_size)
+
+        self.to(device)
+
+    def forward(self, obs, rnn_states, action_masks):#masks, action_masks):
+        actor_features = self.base(obs)
+        actor_features, rnn_states = self.rnn(actor_features, rnn_states)#, masks)
+        actions, action_log_probs = self.act(actor_features, action_masks)
+        return actions, action_log_probs, rnn_states
+
+    def evaluate_actions(self, obs, rnn_states, action, action_masks):#masks, action_masks):
+        actor_features = self.base(obs)
+
+        actor_features, rnn_states = self.rnn(actor_features, rnn_states)#, masks)
+
+        action = torch.tensor(action,dtype=torch.float32)
+       
+        action_log_probs, dist_entropy = self.act.evaluate_actions(actor_features,action)
+
+        return action_log_probs, dist_entropy
+
+
+class Critic(nn.Module):
+    def __init__(self, hidden_size, layer_N, recurrent_N, cent_obs_space, device=torch.device("cpu")):
+        super(Critic, self).__init__()
+        init_method = nn.init.orthogonal_
+
+        cent_obs_shape = get_shape_from_obs_space(cent_obs_space)
+        self.base = MLPBase(hidden_size, cent_obs_shape, layer_N)
+
+        self.rnn = RNNLayer(hidden_size, hidden_size, recurrent_N)
+
+        def init_(m):
+            return init(m, init_method, lambda x: nn.init.constant_(x, 0))
+
+        self.v_out = init_(nn.Linear(hidden_size, 1))
+
+        self.to(device)
+
+    def forward(self, cent_obs, rnn_states):#, masks):
+        critic_features = self.base(cent_obs)
+        critic_features, rnn_states = self.rnn(critic_features, rnn_states)#, masks)
+        values = self.v_out(critic_features)
+
+        return values, rnn_states
---
+++ plugins/marl/mappo/mappo_policy.py
@@ -0,0 +1,267 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+import numpy as np
+from plugins.marl.mappo.actor_critic import Actor, Critic
+from modules.separated_buffer import SeparateReplayBuffer
+from modules.utils import config
+import math
+
+# MAPPOPolicy class encapsulates the MAPPO algorithm for training an agent with a shared policy
+# The code is based on the MAPPO implementation from the repository: https://github.com/marlbenchmark/on-policy/
+
+mappo_config = config['decision_making']['MAPPO']
+
+class MAPPOPolicy:
+
+    """Initialize the MAPPO policy with agent-specific configuration."""
+    def __init__(self, agent):
+        self.agent = agent
+        self.device = torch.device(mappo_config['device'])
+
+        self.clip_param = mappo_config['clip_param']
+        self.ppo_epoch = mappo_config['ppo_epoch']
+        self.num_mini_batch = mappo_config['num_mini_batch']
+        self.value_loss_coef = mappo_config['value_loss_coef']
+        self.entropy_coef = mappo_config['entropy_coef']
+        self.max_grad_norm = mappo_config['max_grad_norm']
+
+        self.learning_rate = mappo_config['lr']
+        self.critic_lr = mappo_config['critic_lr']
+        self.epsilon = mappo_config['epsilon']
+        self.weight_decay = mappo_config['weight_decay']
+        self.hidden_size = mappo_config['hidden_size']
+        self.layer_N = mappo_config['layer_N']
+        self.recurrent_N = mappo_config['recurrent_N']
+        self.gamma = mappo_config['gamma']
+        self.num_mini_batch = mappo_config['num_mini_batch']
+        self.data_chunk_length = mappo_config['data_chunk_length']
+        self.episode_length = mappo_config['episode_length']
+
+        self.inited = False
+       
+    """Initialize components of the MAPPO agent. This is for initialization after other instances initialized."""
+    def init(self):
+        self.local_observation_space = self.agent.rl_agent.local_observation_space
+        self.global_observation_space = self.agent.rl_agent.global_observation_space
+        self.action_space = self.agent.rl_agent.action_space
+        self.buffer = SeparateReplayBuffer(self.episode_length, self.local_observation_space, self.global_observation_space, self.action_space, self.hidden_size, self.recurrent_N, self.gamma)
+
+        self.actor = Actor(self.hidden_size, self.layer_N, self.recurrent_N, self.local_observation_space, self.action_space, self.device)
+        self.critic = Critic(self.hidden_size, self.layer_N, self.recurrent_N, self.global_observation_space, self.device)
+
+
+        self.actor = DDP(self.actor)
+        self.critic = DDP(self.critic)
+
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),
+                                                lr=self.learning_rate, eps=self.epsilon,
+                                                weight_decay=self.weight_decay)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),
+                                                 lr=self.critic_lr,
+                                                 eps=self.epsilon,
+                                                 weight_decay=self.weight_decay)
+        self.inited = True
+
+        self.step = 0
+
+
+    """Calculate returns for the collected data."""
+    @torch.no_grad()
+    def compute(self):
+        self.prep_rollout()
+        next_values = self.get_value(self.buffer.share_obs[-1],
+                                      self.buffer.available_actions[-1])
+        self.buffer.compute_returns(next_values[0])
+       
+    """Insert data into the replay buffer."""
+    def insert(self, data):
+        obs, share_obs, rewards, available_actions, \
+        values, actions, action_log_probs, rnn_states, rnn_states_critic = data
+        rnn_states = np.array(rnn_states)
+        rnn_states_critic = np.array(rnn_states_critic)
+        actions = np.array(actions)
+        action_log_probs = np.array(action_log_probs)
+        values = np.array(values)
+        action_masks = np.array([1 if len(available_actions) > i else 0 for i in range(self.action_space.n-1)])
+        action_masks = np.insert(action_masks, 0, 1)
+
+        self.buffer.insert(share_obs, obs, rnn_states, rnn_states_critic,
+                           actions, action_log_probs, values, rewards, action_masks)
+
+    """Prepare for training phase (set networks to train mode)."""
+    def prep_training(self):
+        self.actor.train()
+        self.critic.train()
+
+    """Prepare for rollout phase (set networks to evaluation mode)."""
+    def prep_rollout(self):
+        self.actor.eval()
+        self.critic.eval()
+
+    """Evaluate actions taken by the agent, calculate log probabilities and value."""
+    def evaluate_actions(self, cent_obs, obs, rnn_states_actor, rnn_states_critic, action, 
+                         available_actions=None):
+        action_log_probs, dist_entropy = self.actor.module.evaluate_actions(obs,
+                                                                     rnn_states_actor,
+                                                                     action,
+                                                                     available_actions)
+
+        values, _ = self.critic(cent_obs, rnn_states_critic)
+        return values, action_log_probs, dist_entropy
+
+    """Compute the gradient norm for gradient clipping."""
+    def get_grad_norm(self, it):
+        sum_grad = 0
+        for x in it:
+            if x.grad is None:
+                continue
+            sum_grad += x.grad.norm() ** 2
+        return math.sqrt(sum_grad)
+
+    """Compute mean squared error loss."""
+    def mse_loss(self, e):
+        return e**2/2
+
+    """Calculate the value loss for critic update."""
+    def cal_value_loss(self, values, value_preds_batch, return_batch):
+        value_pred_clipped = value_preds_batch + (values - value_preds_batch).clamp(-self.clip_param,
+                                                                                        self.clip_param)
+        error_clipped = return_batch - value_pred_clipped
+        error_original = return_batch - values
+
+        value_loss_clipped = self.mse_loss(error_clipped)
+        value_loss_original = self.mse_loss(error_original)
+
+        value_loss = value_loss_original
+
+        value_loss = value_loss.mean()
+
+        return value_loss
+
+    """Perform PPO update for both actor and critic."""
+    def ppo_update(self, sample):
+        share_obs_batch, obs_batch, rnn_states_batch, rnn_states_critic_batch, actions_batch, \
+        value_preds_batch, return_batch, old_action_log_probs_batch, \
+        adv_targ, available_actions_batch = sample
+
+        # Reshape to do in a single forward pass for all steps
+        values, action_log_probs, dist_entropy = self.evaluate_actions(share_obs_batch,
+                                                                       obs_batch,
+                                                                       rnn_states_batch,
+                                                                       rnn_states_critic_batch,
+                                                                       actions_batch,
+                                                                       available_actions_batch)
+
+        # actor update
+        old_action_log_probs_batch = torch.tensor(old_action_log_probs_batch,dtype=torch.float32)
+        imp_weights = torch.exp(action_log_probs - old_action_log_probs_batch)
+        
+        adv_targ = torch.tensor(adv_targ,dtype=torch.float32)
+        surr1 = imp_weights * adv_targ
+        surr2 = torch.clamp(imp_weights, 1.0 - self.clip_param, 1.0 + self.clip_param) * adv_targ
+
+        policy_action_loss = (-torch.sum(torch.min(surr1, surr2),
+                                         dim=-1,
+                                         keepdim=True)).sum()
+
+        policy_loss = policy_action_loss
+
+        self.actor_optimizer.zero_grad()
+
+        (policy_loss - dist_entropy * self.entropy_coef).backward()
+
+        actor_grad_norm = self.get_grad_norm(self.actor.parameters())
+
+        self.actor_optimizer.step()
+
+        # critic update
+        value_preds_batch = torch.tensor(value_preds_batch, dtype=torch.float32)
+        return_batch = torch.tensor(return_batch, dtype=torch.float32)
+        value_loss = self.cal_value_loss(values, value_preds_batch, return_batch)
+
+        self.critic_optimizer.zero_grad()
+
+        (value_loss * self.value_loss_coef).backward()
+
+        critic_grad_norm = self.get_grad_norm(self.critic.parameters())
+        self.critic_optimizer.step()
+
+        return value_loss, critic_grad_norm, policy_loss, dist_entropy, actor_grad_norm, imp_weights
+
+    """Train the agent using the PPO update."""
+    def train(self):
+        advantages = self.buffer.returns[:-1] - self.buffer.value_preds[:-1]
+        advantages_copy = advantages.copy()
+        mean_advantages = np.nanmean(advantages_copy)
+        std_advantages = np.nanstd(advantages_copy)
+        advantages = (advantages - mean_advantages) / (std_advantages + 1e-5)
+
+        for _ in range(self.ppo_epoch):
+            data_generator = self.buffer.recurrent_generator(advantages, self.num_mini_batch, self.data_chunk_length)
+
+            for sample in data_generator:
+                value_loss, critic_grad_norm, policy_loss, dist_entropy, actor_grad_norm, imp_weights \
+                    = self.ppo_update(sample)
+                
+                if self.agent.agent_id == 0:
+                    print(policy_loss)
+
+        self.buffer.after_update()
+
+    """Collect actions and values for the agent."""
+    @torch.no_grad()
+    def collect(self, blackboard):
+        available_actions = blackboard['closest_tasks']
+        action, action_log_prob, rnn_state = self.get_action(blackboard, available_actions)
+        value, rnn_state_critic = self.get_value(blackboard['global_observation'], available_actions)
+
+        return action, action_log_prob, rnn_state, value, rnn_state_critic
+
+    """Decide on an action based on the current state."""
+    def decide(self, blackboard) -> int:
+        if self.inited == False:
+            self.init()
+        available_actions = blackboard['closest_tasks']
+        action, action_log_prob, rnn_state, value, rnn_state_critic = self.collect(blackboard)
+        if len(blackboard['closest_tasks']) > action - 1 and action != 0:
+            selected_task = blackboard['closest_tasks'][action-1]
+            selected_task_id = selected_task.task_id
+        else:
+            selected_task_id = None
+
+
+        data = blackboard['local_observation'], blackboard['global_observation'],\
+               blackboard['reward'], available_actions, \
+               value, action, action_log_prob, rnn_state, rnn_state_critic
+
+        self.insert(data)
+        self.step = self.step + 1
+
+        if self.step is self.episode_length:
+            self.compute()
+            self.prep_training()
+            self.train()
+            self.prep_rollout()
+            self.step = 0
+        
+        return selected_task_id
+
+    """Get action based on the current state and available actions."""
+    def get_action(self, blackboard, available_actions):
+        action_masks = [1 if len(available_actions) > i else 0 for i in range(self.action_space.n - 1)]
+        action_masks = np.insert(action_masks, 0, 1)
+        prev_rnn_state = self.buffer.rnn_states[-1]
+        action, action_log_prob, rnn_state = self.actor(obs = blackboard['local_observation'],
+                                                        rnn_states = prev_rnn_state,
+                                                        action_masks = action_masks)
+        return action, action_log_prob, rnn_state
+        
+
+    def get_value(self, cent_obs, available_actions):
+        prev_rnn_state = self.buffer.rnn_states_critic[-1]
+        value, rnn_state = self.critic(cent_obs = cent_obs, 
+                                rnn_states = prev_rnn_state)
+        return value, rnn_state
