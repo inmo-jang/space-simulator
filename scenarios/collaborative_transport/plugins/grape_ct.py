8f077e98a4ab473667072e2c8ba8f2438c75a56e
yupyup-Ahyeon <yupyupman2001@gmail.com>
2025-05-28 22:35:32 +0900

    updated utility computing with waiting time to use deficiency_ratio and try/except

--- scenarios/collaborative_transport/plugins/grape_ct.py
+++ scenarios/collaborative_transport/plugins/grape_ct.py
@@ -1,10 +1,13 @@
 import math
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..')))
+from modules.utils import config
 
 from plugins.grape.grape import *
 
-WAITING_TIME_FACTOR = 1.1   # 임시로 지정
+WAITING_TIME_TOLERANCE = config['decision_making']['CT']['waiting_time_tolerance']
+MIN_WAITING_FACTOR = config['decision_making']['CT']['min_waiting_factor']
+MIN_WAITING_TIME = MIN_WAITING_FACTOR * WAITING_TIME_TOLERANCE
 
 class GRAPE_CT(GRAPE):
     def __init__(self, agent):
@@ -28,11 +31,17 @@ class GRAPE_CT(GRAPE):
         # Default Utility
         utility = task.amount / (num_collaborator) - COST_WEIGHT_FACTOR * distance * (num_collaborator ** SOCIAL_INHIBITION_FACTOR)         
 
-        # GRAPE with waiting time
-        waiting_time_term = math.pow(WAITING_TIME_FACTOR, max_waiting_time)
-        if task.num_sides >= num_collaborator:
-            utility = (task.amount + waiting_time_term)/num_collaborator - distance
-        else:
-            utility = float('-inf')
+        # GRAPE with waiting time        
+        deficiency_ratio = (task.num_sides - len(task.ready_agents))/task.num_sides # 낮을 수록 이미 많은 agents가 모였다는 것임. 
+        deficiency_ratio += 0.1
+        deficiency_ratio_inverse = 1/deficiency_ratio
+
+        try:
+            virtual_amount = math.pow(deficiency_ratio_inverse, waiting_time / WAITING_TIME_TOLERANCE)
+        except OverflowError:
+            utility = float('inf')
+            return utility
+
+        utility = (task.amount + virtual_amount)/num_collaborator - COST_WEIGHT_FACTOR * distance
 
         return utility
