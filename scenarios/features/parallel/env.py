c2ef93fff832e31f60c88614d7b5263fb28e6c40
Inmo Jang <inmo3592@gmail.com>
2025-08-14 02:01:46 +0900

    Minor revision from simple to features.parallel

--- scenarios/features/parallel/env.py
+++ scenarios/features/parallel/env.py
@@ -1,7 +1,7 @@
 from modules.base_env import BaseEnv
 from modules.utils import ResultSaver
-from scenarios.simple.task import generate_tasks
-from scenarios.simple.agent import generate_agents
+from scenarios.features.parallel.task import generate_tasks
+from scenarios.features.parallel.agent import generate_agents
 import pygame
 
 class Env(BaseEnv):
