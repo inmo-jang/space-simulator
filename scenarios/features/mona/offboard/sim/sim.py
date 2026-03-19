f0fbd6de238a2159a1a6271dcdc81cda3343fab6
Chaeyoung1011 <danielim2174@gmail.com>
2026-03-19 17:55:22 +0900

    Quit시 결과 저장, task_amount done음수 시  0으로 고정, 결과 저장 후 quit

--- scenarios/features/mona/offboard/sim/sim.py
+++ scenarios/features/mona/offboard/sim/sim.py
@@ -76,7 +76,29 @@ class Sim(BaseSim):
             agent.mona_comm = self.mona_comm
 
     def save_results(self):
-        pass
+        # Save gif
+        if self.save_gif and self.rendering_mode == "Screen":        
+            self.recording = False
+            print("Recording stopped.")
+            self.result_saver.save_gif(self.frames)          
+     
+
+        # Save time series data
+        if self.save_timewise_result_csv:        
+            csv_file_path = self.result_saver.save_to_csv("timewise", self.data_records, ['time', 'agents_total_distance_moved', 'agents_total_task_amount_done', 'remaining_tasks', 'tasks_total_amount_left'])          
+            self.result_saver.plot_timewise_result(csv_file_path)
+        
+        # Save agent-wise data            
+        if self.save_agentwise_result_csv:        
+            variables_to_save = ['agent_id', 'task_amount_done', 'distance_moved']
+            agentwise_results = self.result_saver.get_agentwise_results(self.agents, variables_to_save)                        
+            csv_file_path = self.result_saver.save_to_csv('agentwise', agentwise_results, variables_to_save)
+            
+            self.result_saver.plot_boxplot(csv_file_path, variables_to_save[1:])
+
+        # Save yaml
+        if self.save_config_yaml:                
+            self.result_saver.save_config_yaml()   
 
     def record_timewise_result(self):
         agents_total_distance_moved = sum(agent.distance_moved for agent in self.agents)
